4d41628109e95b2c9458405aabb5712f26d3640c
JieLuo <jieluo129@gmail.com>
2022-08-20 22:34:16 +0800

    add a func named mian_start.

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
-using std::cout;
+#include <stdio.h>
+
+int mian_start(){
+    
+    printf("main start...\n");
+    return 2;
+}
 
 int main(){
 
-    cout << "hello, world.\n";
+    int ret = 0;
+    ret = mian_start();
+
+    if (ret != 1) {
+        printf("main start fail!\n");
+    }
+    else {
+        printf("mian start success!\n");
+    }
+
     return 0;
 }
