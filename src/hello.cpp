4aaf224226c4ca9e3041b56f3220a924fc3f908a
JieLuo <jieluo129@gmail.com>
2022-08-22 21:10:54 +0800

    some Docs.

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,21 +1,35 @@
 #include <stdio.h>
 
-int mian_start() {
+/**
+ * @brief main()函数启动时运行的函数
+ *
+ * @return int 固定返回2
+ */
+int Main_Start() {
   printf("main start...\n");
   return 2;
 }
 
+/**
+ * @brief main函数, 主函数
+ *
+ * @return int  固定返回1
+ */
 int main() {
+  /**
+   * @brief 存储Main_Start函数的返回值
+   *
+   */
   int ret = 0;
-  ret = mian_start();
+  ret = Main_Start();
 
   if (ret != 1) {
-    printf("main start fail!\n");
+    printf("main start fail!\n");  ///  启动失败
   } else {
-    printf("mian start success!\n");
+    printf("mian start success!\n");  ///   启动成功
   }
 
-  printf("test a cursor \n");
+  printf("test a cursor.\n");
   printf("just a test.\n");
 
   return 0;
